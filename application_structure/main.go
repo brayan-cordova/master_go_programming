087afd5e871e399d215caacb7712418423b5378b
Bosnan <brayancordova9900@gmail.com>
2023-12-20 18:17:32 -0600

    Adding comments to Structure of a Go Application

--- application_structure/main.go
+++ application_structure/main.go
@@ -1,12 +1,21 @@
+// main is a special name declaring an executable rather than a library (package)
 package main
 
+// import declaration declares packages used in this file
 import (
 	"fmt"
 )
 
+// package scoped variables and constants
 const secondsInHour = 3600
 
+// a function declaration. main is a special function name
+// it is the entry point for the executable program
+// Go uses brace brackets to delimit code blocks
 func main(){
+	
+    // Println() function prints out a line to stdout
+    // It belongs to package fmt
 	fmt.Println("Hello Go World...!!")
 	distance := 60.8 // distance in km
 	fmt.Printf("The distance in miles is %f \n", distance * 0.621)
